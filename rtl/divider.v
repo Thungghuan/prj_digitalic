90ed2924f5105b9934e5d3bf8ddd4c97474c830e
Thungghuan <1092887426@qq.com>
2022-06-14 14:49:33 +0800

    feat: delele unused reg

--- rtl/divider.v
+++ rtl/divider.v
@@ -3,14 +3,11 @@ module divider_step(
     dividend,
     divisor,
     quotient_last,
-    result_shift_last,
 
     quotient,
-    remainder,
-    result_shift
+    remainder
 );
 
-parameter step = 0;
 parameter M = 26; // bit number of dividend
 parameter N = 14; // bit number of divisor
 
@@ -18,15 +15,13 @@ input clk;
 input [N:0] dividend;
 input [N - 1:0] divisor;
 input [M - 1:0] quotient_last;
-input [N:0] result_shift_last;
 
 output reg [M - 1:0] quotient;
 output reg [N - 1:0] remainder;
-output reg [N:0] result_shift;
 
-reg [N:0] count = 0;
+reg [N:0] count;
 reg [N:0] divisor_t;
-reg overflow = 0;
+reg overflow;
 
 always @(posedge clk) begin
     divisor_t = {1'b0, divisor};
@@ -61,11 +56,6 @@ always @(posedge clk) begin
         quotient <= quotient_last << 1;
         remainder <= dividend;
     end
-
-    if (step == 0)
-        result_shift <= count;
-    else
-        result_shift <= result_shift_last;
 end
 
 endmodule
@@ -80,7 +70,6 @@ input [M - 1:0] dividend;
 input [N - 1:0] divisor;
 output [M - 1:0] quotient;
 
-wire [N:0] result_shifts [M - N - 1:0];
 wire [M - 1:0] quotients [M - N - 1:0];
 wire [N - 1:0] remainders [M - N - 1:0];
 
@@ -89,28 +78,23 @@ divider_step step_0(
     .dividend(dividend[M - 1:M - N - 1]),
     .divisor(divisor),
     .quotient_last({M{1'b0}}),
-    .result_shift_last({(N + 1){1'b0}}),
 
     .quotient(quotients[M - N - 1]),
-    .remainder(remainders[M - N - 1]),
-    .result_shift(result_shifts[M - N - 1])
+    .remainder(remainders[M - N - 1])
 );
 
 genvar i;
 generate
     for (i = 1; i <= M - N - 1; i = i + 1) begin
-        divider_step #(i)
-        step_i 
+        divider_step step_i 
         (
             .clk(clk),
             .dividend({remainders[M - N - i], dividend[M - N - i]}),
             .divisor(divisor),
             .quotient_last(quotients[M - N - i]),
-            .result_shift_last(result_shifts[M - N - i]),
 
             .quotient(quotients[M - N - 1 - i]),
-            .remainder(remainders[M - N - 1 - i]),
-            .result_shift(result_shifts[M - N - 1 - i])
+            .remainder(remainders[M - N - 1 - i])
         );
     end
 endgenerate
