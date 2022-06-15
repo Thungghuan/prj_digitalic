88929282887d9e0588b465bc7d5c832d82eb7f0f
Thungghuan <1092887426@qq.com>
2022-06-15 16:40:49 +0800

    feat: update define.tcl

--- tcl/define.tcl
+++ tcl/define.tcl
@@ -1,8 +1,11 @@
-
-set DESIGN_NAME "module_sin"
+set DESIGN_NAME "module_top"
 
 set rtl_dir "./rtl"
 
 set RTL_SOURCE_FILES " \
-${rtl_dir}/module_sin.v \
+${rtl_dir}/module_top.v \
+${rtl_dir}/sin.v \
+${rtl_dir}/divider.v \
+${rtl_dir}/multiplier.v \
+${rtl_dir}/s2p.v \
 "
